43d7cc3de48ebd75a8e67266b4e077ab955f5d31
ki11errabbit <unlikelytitan@gmail.com>
2022-02-14 20:41:12 -0700

    Init commit

--- config.h
+++ config.h
@@ -1,16 +1,20 @@
 #define CMDLENGTH 60
-#define DELIMITER "  "
+#define DELIMITER " | "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	/*Command*/             /*Update Interval*/     /*Update Signal*/
+	//BLOCK("sb-mail",    1800, 17),
+	//BLOCK("sb-music",   0,    18),
+	//BLOCK("sb-disk",    1800, 19),
+	//BLOCK("sb-memory",  10,   20),
+	//BLOCK("sb-loadavg", 5,    21),
+	//BLOCK("sb-mic",     0,    26),
+	//BLOCK("sb-record",  0,    27),
+	BLOCK("~/.dwm/blocks-scripts/weather", 18000, 5),
+	BLOCK("~/.dwm/blocks-scripts/volume",  0,    10),
+	BLOCK("free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 5, 0),
+	BLOCK("~/.dwm/blocks-scripts/clock", 5, 12),
+	//BLOCK("sb-battery", 5,    23),
+	//BLOCK("sb-date",    1,    24)
 };
